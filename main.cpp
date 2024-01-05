02ee5747f4487d9713cbfde5e2dffb0be3145c1c
Lucca Emanuelli <emanuellilucca@gmail.com>
2024-01-05 16:00:40 -0300

    list of all registered students

--- main.cpp
+++ main.cpp
@@ -4,25 +4,55 @@ using namespace std;
 
 int main()
 {
-    int students;
-    cout << "STUDENTS REGISTERED: " << endl;
+  int students;
+  cout << "Students registered: ";
+  cin >> students;
+  
+  Student *s1[students];
+  
+  for (int i = 0; i < students; i++)
+  {
+    string name;
+    int age, regis;
+    cout << "\n\n===== STUDENT REGISTRATION =====" << endl;
+    
+    cout << "Name: ";
+    cin >> name;
 
-    Student *s1[students];
+    cout << "\nAge: ";
+    cin >> age;
 
-    for (int i = 0; i < students; i++)
+    cout << "\nRegistration: ";
+    cin >> regis;
+
+    s1[i] = new Student(name, age, regis);
+  }
+  for (int i = 0; i < 20; i++)
+  {
+    int option, reg;
+    cout << "\n\nDo you want to check any student data?\n1-Yes\n2-List all\n3-Exit ... ";
+    cin >> option;
+
+    if (option == 1)
     {
-        int regis, age;
-        string name;
-
-        cout << "NAME: ";
-        cin >> name;
-
-        cout << "AGE: ";
-        cin >> age;
-
-        cout << "REGISTRATION: ";
-        cin >> regis;
-
-        s1[i] = new Student(name, age, regis);
+      cout << "Type registration number: ";
+      cin >> reg;
+      for(int k = 0; k < students; k++)
+      {
+        if (reg == s1[k]->getRegistration())
+        {
+          cout << "\nName: " << s1[k]->getName() << "\nAge: " << s1[k]->getAge() << endl;
+        }
+      }
+    } else if (option == 2) 
+    {
+      for (int k = 0; k < students; k++)
+      {
+        cout << "\nName: " << s1[k]->getName() << "\nAge: " << s1[k]->getAge() << endl;
+      }
+    } else if (option == 3)
+    {
+      return 0;
     }
+  }
 }
